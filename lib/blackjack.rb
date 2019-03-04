ef1c46c039327356514452d4dcb40039282ad344
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-03-04 16:25:37 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,49 @@
 def welcome
-  # code #welcome here
+ puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card)
+  puts "Your cards add up to #{card}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+ input = gets.chomp
+ return input
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+    puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+card1 = deal_card 
+card2 = deal_card 
+ display_card_total(card1+card2)
+ return card1+card2
 end
 
-def hit?
-  # code hit? here
+def hit?(number)
+  prompt_user
+  input=get_user_input
+  if input == 's'
+    number
+    elsif input == 'h'
+  new_card=deal_card  
+  number+new_card
+  else invalid_command 
+end
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command."
 end
 
 #####################################################
@@ -39,6 +51,12 @@ end
 #####################################################
 
 def runner
-  # code runner here
+ welcome
+ score=initial_round
+ until score > 21
+ score = hit?(score)
+ display_card_total(score)
+ end
+ end_game(score)
 end
     
